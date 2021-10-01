5049ce8ea3ce0628000b874cc1ac6479bf64c2fb
Idris Dose <idrisdev@gmail.com>
2021-10-01 19:40:23 +1000

    update (qbcore): Removed import.lua and replaced with exports.

--- client/main.lua
+++ client/main.lua
@@ -1,3 +1,4 @@
+local QBCore = exports['qb-core']:GetCoreObject()
 local isLoggedIn = LocalPlayer.state['isLoggedIn']
 local playerJob = nil
 
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,11 +4,7 @@ game 'gta5'
 description 'QB-GarbageJob V2'
 version '1.0.0'
 
-shared_scripts {
-	'@qb-core/import.lua',
-	'config.lua'
-}
-
+shared_script 'config.lua'
 client_script 'client/main.lua'
 server_script 'server/main.lua'
 
