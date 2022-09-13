46322e85442ce09d142fd99952ba40cee055c075
buddizer <iw3io8ge@duck.com>
2022-09-13 22:54:10 +0000

    missing langs

--- locales/fr.lua
+++ locales/fr.lua
@@ -37,7 +37,11 @@ local Translations = {
     },
     warning = {},
 }
-Lang = Locale:new({
-    phrases = Translations,
-    warnOnMissing = true
-})
+
+if GetConvar('qb_locale', 'en') == 'fr' then
+    Lang = Locale:new({
+        phrases = Translations,
+        warnOnMissing = true,
+        fallbackLang = Lang,
+    })
+end
